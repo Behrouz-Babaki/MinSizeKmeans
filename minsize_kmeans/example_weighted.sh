29b6bb14a98cbb431d0413583b80820d5cfb1f24
Ofir Reich <e.scribbler@gmail.com>
2018-01-28 18:14:56 +0200

    Update example_weighted.sh
    Make the second example write to a different file.

--- minsize_kmeans/example_weighted.sh
+++ minsize_kmeans/example_weighted.sh
@@ -19,5 +19,5 @@
 # The total weight of points in each cluster should be at most 4000
 # Run the algorithm 2 times and ouput the best clustering
 # Store the results in the file wmm.out
-./weighted_mm_kmeans.py ../data/iris.data 3 ../data/iris.weights 2000 4000 -n 2 -o wmm.out
+./weighted_mm_kmeans.py ../data/iris.data 3 ../data/iris.weights 2000 4000 -n 2 -o wmm2.out
 
